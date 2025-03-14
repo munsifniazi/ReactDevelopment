f0acbaf481ec41a89e9330f0907fd6be652a2099
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-15 01:13:05 +0500

    Update App.jsx

--- ReactBasics/src/App.jsx
+++ ReactBasics/src/App.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import NetflixSeries from './components/NetflixSeries';
+import NetflixSeries, { Header, Footer } from './components/NetflixSeries';
 const App = () => {
   return (
     <>
-      <h1>Netflix Series</h1>
+      {/* <h1>Netflix Series</h1> */}
+      <Header />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
+      <Footer />
     </>
   );
 };
 export default App;
 
-
-
-
-
-
-
-
-
-{/* <div>
+{
+  /* <div>
 <img src='image.jpg' alt='The Hunting Party' />
 </div>
 <h1>Name : The Hunting Party</h1>
@@ -30,4 +25,5 @@ export default App;
 Description : Lorem ipsum dolor sit amet consectetur adipisicing elit.
 Similique consequuntur nobis optio. Eius fugit delectus officiis sint
 quod sit modi ut voluptatum.
-</p> */}
+</p> */
+}
