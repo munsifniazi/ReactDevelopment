560eee71c39574e95c75ea8dce2e733aad8d10cc
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-17 01:41:32 +0500

    Update main.jsx

--- 02React/src/main.jsx
+++ 02React/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Profile from './components/Profile'
-// import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Profile from './components/Profile';
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Profile />
+    <App />
+    {/* <Profile /> */}
   </StrictMode>,
-)
+);
