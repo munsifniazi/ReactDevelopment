de0bab793ebd760bf686641b2a553aec7e4b1cd5
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-16 05:01:58 +0500

    Update NetflixSeries.jsx

--- 02React/src/components/NetflixSeries.jsx
+++ 02React/src/components/NetflixSeries.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import seriesData from '../api/seriesData.json';
+import { SeriesData } from './SeriesData';
 const NetflixSeries = () => {
   return (
     <>
       <ul>
         {seriesData.map((curElem) => {
-          return (
-            <li key={curElem.id}>
-              <div>
-                <img
-                  src={curElem.img_url}
-                  alt='Yeh kyn ni chal raha'
-                  width='40%'
-                  height='40%'
-                />
-              </div>
-              <h1>Name : {curElem.name}</h1>
-              <p>Rating : {curElem.rating}</p>
-              <p>Description : {curElem.description}</p>
-              <p>Genre : {curElem.genre}</p>
-              <p>Cast : {curElem.cast} </p>
-              <a href={curElem.watch_url} target='_blank'>
-                <button>Watch Now</button>
-              </a>
-            </li>
-          );
+          return  <SeriesData key={curElem.id} data = {curElem} />
+    
         })}
       </ul>
     </>
