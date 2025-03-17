ac7075f5cc4d0bf5d0db15474e2a25041aa2d676
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-17 06:00:07 +0500

    Update App.jsx

--- 02React/src/App.jsx
+++ 02React/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import NetflixSeries from './components/NetflixSeries';
+import EventsHandling from './components/EventsHandling';
+import EventProps from './EventProps';
+import EventPrac from './EventPrac';
+import {EventPropagation} from './EventPropagation';
+// import NetflixSeries from './components/NetflixSeries';
 // import Profile from './components/Profile';
-import './components/Netflix.css';
+// import './components/Netflix.css';
 
 const App = () => {
   return (
     <section className='container'>
-      <h1 className='card-heading'> List of Netflix Series</h1>
-      <NetflixSeries />
+      {/* <h1 className='card-heading'> List of Netflix Series</h1> */}
+      {/* <NetflixSeries /> */}
       {/* <Profile />   */}
+      {/* <EventsHandling /> */}
+      {/* <EventProps /> */}
+      {/* <EventPrac /> */}
+      <EventPropagation />
     </section>
   );
 };
