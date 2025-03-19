2f52cd5e0c3dba180fb04dd68e3fa40dbec39fbc
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-19 11:46:40 +0500

    Update App.jsx

--- 02React/src/App.jsx
+++ 02React/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import EventsHandling from './components/EventsHandling';
-import EventProps from './EventProps';
-import EventPrac from './EventPrac';
-import { EventPropagation } from './EventPropagation';
-import State from './components/hooks/State';
+// import EventsHandling from './components/EventsHandling';
+// import EventProps from './EventProps';
+// import EventPrac from './EventPrac';
+// import { EventPropagation } from './EventPropagation';
+// import State from './components/hooks/State';
 // import NetflixSeries from './components/NetflixSeries';
 // import Profile from './components/Profile';
 // import './components/Netflix.css';
-import './components/EV.css';
-import DerivedState from './components/hooks/DerivedState';
-import PracHooks from './components/hooks/PracHooks';
-import LiftingState from './components/hooks/LiftingState';
-import ToggleSwitch from './Projects/ToggleSwitch/ToggleSwitch';
+// import './components/EV.css';
+// import DerivedState from './components/hooks/DerivedState';
+// import PracHooks from './components/hooks/PracHooks';
+// import LiftingState from './components/hooks/LiftingState';
+// import ToggleSwitch from './Projects/ToggleSwitch/ToggleSwitch';
+import Todo from './Projects/Todo/Todo';
 
 const App = () => {
   return (
@@ -27,7 +28,8 @@ const App = () => {
       {/* <DerivedState/> */}
       {/* <PracHooks /> */}
       {/* <LiftingState /> */}
-      <ToggleSwitch/>
+      {/* <ToggleSwitch/> */}
+      <Todo/>
     </section>
   );
 };
