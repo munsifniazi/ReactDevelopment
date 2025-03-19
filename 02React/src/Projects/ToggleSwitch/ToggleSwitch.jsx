ae4cda54c4319c7c054017d62a411117940ef430
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-19 11:47:10 +0500

    Update ToggleSwitch.jsx

--- 02React/src/Projects/ToggleSwitch/ToggleSwitch.jsx
+++ 02React/src/Projects/ToggleSwitch/ToggleSwitch.jsx
@@ -13,6 +13,7 @@ const ToggleSwitch = () => {
       <div className='toggle-switch' onClick={handleToggleSwitch } style={toggleBGColor}>
         <div className={`switch ${checkIson}`}>
           <span className='switch-state'> { checkIson} </span>
+          
         </div>
       </div>
     </>
