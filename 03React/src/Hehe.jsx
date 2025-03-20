887f4f0d5d09abee2ae35777d97b616156d5e5f7
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-20 07:22:09 +0500

    Create Hehe.jsx

---
+++ 03React/src/Hehe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Hehe = () => {
+  return (
+    <div>
+      <h1>Hello from Hehe component</h1>
+      <p>This is a test component for checking general file Export.</p>
+      <button>Click Me</button>
+    </div>
+  );
+};
