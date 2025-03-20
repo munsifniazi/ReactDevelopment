f8610dca52151c3d3ef6644bd577ca0dc723e39d
munsifniazi <158184200+munsifniazi@users.noreply.github.com>
2025-03-20 07:20:07 +0500

    Starting file of 03React

---
+++ 03React/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Intro from "./Intro";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Code With Munsif Khan</h1>
+      <p>Welcome to the React Tutorial.</p>
+      <p>This is a simple React application.</p>
+      <p>Developed by Munsif Khan</p>
+      <p>React is a JavaScript library for building user interfaces.</p>
+      <p>React is used to build single page applications.</p>
+      <p>React allows us to create reusable UI components.</p>
+      <p>React is created by Facebook.</p>
+      <p>React is used by Netflix, BBC, Instagram, and many more...</p>
+      <Intro />
+    </div>
+  );
+};
+
+export default App;
---
+++ 03React/src/Intro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <>
+      <p>Yeh mera Intro hai Hehehe.</p>
+    </>
+  );
+};
+
+export default Intro;
